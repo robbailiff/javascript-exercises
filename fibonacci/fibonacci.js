8e49cbe580732aeb8f5e6c8bd1a7883e3eeb0744
robbailiff <robbailiff@gmail.com>
2020-12-10 16:43:45 +0000

    Completed the 4th and 5th exercises of JS Fundamentals 5

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if(n <= 0){
+        return "OOPS";
+    } else if(n <= 1){
+        return 1;
+    } else {
+        let x = 0;
+        let y = 1;
+        let total;
+        for(let i = 2; i <= n; i++){
+            total = x + y;
+            x = y;
+            y = total;
+        }
+        return total;
+    }
 }
 
 module.exports = fibonacci
--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    let bookList = [];
+    arr.forEach(book => {
+        bookList.push(book.title);
+    });
+    return bookList;
 }
 
 module.exports = getTheTitles;
