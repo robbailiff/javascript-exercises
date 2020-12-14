42c14294b73f3c7fdbaa31d64ce5bc0b58b34359
robbailiff <robbailiff@gmail.com>
2020-12-14 19:42:19 +0000

    Completed findTheOldest exercise

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-let findTheOldest = function() {
-
+let findTheOldest = function(arr) {
+    return arr.reduce((oldest, current) => {
+		if(!oldest){
+            return (current.yearOfDeath - current.yearOfBirth)
+        } else if(!oldest.yearOfDeath) {
+            const currentYear = new Date().getFullYear();
+            const oldestAge = (currentYear - oldest.yearOfBirth)
+			const currentAge = (current.yearOfDeath - current.yearOfBirth)
+			return oldestAge > currentAge ? oldest : current
+        } else if (!current.yearOfDeath) {
+            const currentYear = new Date().getFullYear();
+            const oldestAge = (oldest.yearOfDeath - oldest.yearOfBirth)
+			const currentAge = (currentYear - current.yearOfBirth)
+			return oldestAge > currentAge ? oldest : current
+		} else {
+			const oldestAge = (oldest.yearOfDeath - oldest.yearOfBirth)
+			const currentAge = (current.yearOfDeath - current.yearOfBirth)
+			return oldestAge > currentAge ? oldest : current
+		}
+    })
 }
 
 module.exports = findTheOldest
